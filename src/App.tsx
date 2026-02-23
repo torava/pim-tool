3d7b8df05e6bcccb62aee3218247393d34c0ff1c
Teemu Orava <teemu.orava@kapsi.fi>
2026-02-23 15:59:05 +0200

    Add hierarchy 4h

--- src/App.tsx
+++ src/App.tsx
@@ -9,56 +9,9 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import TableSortLabel from '@mui/material/TableSortLabel';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import FilterListIcon from '@mui/icons-material/FilterList';
 import { visuallyHidden } from '@mui/utils';
-
-interface Data {
-  id: number;
-  calories: number;
-  carbs: number;
-  fat: number;
-  name: string;
-  protein: number;
-}
-
-function createData(
-  id: number,
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-): Data {
-  return {
-    id,
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-  };
-}
-
-const rows = [
-  createData(1, 'Cupcake', 305, 3.7, 67, 4.3),
-  createData(2, 'Donut', 452, 25.0, 51, 4.9),
-  createData(3, 'Eclair', 262, 16.0, 24, 6.0),
-  createData(4, 'Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData(5, 'Gingerbread', 356, 16.0, 49, 3.9),
-  createData(6, 'Honeycomb', 408, 3.2, 87, 6.5),
-  createData(7, 'Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData(8, 'Jelly Bean', 375, 0.0, 94, 0.0),
-  createData(9, 'KitKat', 518, 26.0, 65, 7.0),
-  createData(10, 'Lollipop', 392, 0.2, 98, 0.0),
-  createData(11, 'Marshmallow', 318, 0, 81, 2.0),
-  createData(12, 'Nougat', 360, 19.0, 9, 37.0),
-  createData(13, 'Oreo', 437, 18.0, 63, 4.0),
-];
+import ArrowUpward from '@mui/icons-material/ArrowUpward';
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
   if (b[orderBy] < a[orderBy]) {
@@ -76,8 +29,8 @@ function getComparator<Key extends PropertyKey>(
   order: Order,
   orderBy: Key,
 ): (
-  a: { [key in Key]: number | string },
-  b: { [key in Key]: number | string },
+  a: { [key in Key]: number | string | null },
+  b: { [key in Key]: number | string | null },
 ) => number {
   return order === 'desc'
     ? (a, b) => descendingComparator(a, b, orderBy)
@@ -86,62 +39,31 @@ function getComparator<Key extends PropertyKey>(
 
 interface HeadCell {
   disablePadding: boolean;
-  id: keyof Data;
+  id: string;
   label: string;
   numeric: boolean;
 }
 
-const headCells: readonly HeadCell[] = [
-  {
-    id: 'name',
-    numeric: false,
-    disablePadding: true,
-    label: 'Dessert (100g serving)',
-  },
-  {
-    id: 'calories',
-    numeric: true,
-    disablePadding: false,
-    label: 'Calories',
-  },
-  {
-    id: 'fat',
-    numeric: true,
-    disablePadding: false,
-    label: 'Fat (g)',
-  },
-  {
-    id: 'carbs',
-    numeric: true,
-    disablePadding: false,
-    label: 'Carbs (g)',
-  },
-  {
-    id: 'protein',
-    numeric: true,
-    disablePadding: false,
-    label: 'Protein (g)',
-  },
-];
-
 interface EnhancedTableProps {
-  onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
+  headCells: HeadCell[];
+  onRequestSort: (event: React.MouseEvent<unknown>, property: string) => void;
   order: Order;
-  orderBy: string;
+  orderBy?: string;
   rowCount: number;
 }
 
 function EnhancedTableHead(props: EnhancedTableProps) {
-  const { order, orderBy, onRequestSort } =
+  const { headCells, order, orderBy, onRequestSort } =
     props;
   const createSortHandler =
-    (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
+    (property: string) => (event: React.MouseEvent<unknown>) => {
       onRequestSort(event, property);
     };
 
   return (
     <TableHead>
       <TableRow>
+        <TableCell />
         {headCells.map((headCell) => (
           <TableCell
             key={headCell.id}
@@ -167,41 +89,17 @@ function EnhancedTableHead(props: EnhancedTableProps) {
     </TableHead>
   );
 }
-function EnhancedTableToolbar() {
-  return (
-    <Toolbar
-      sx={[
-        {
-          pl: { sm: 2 },
-          pr: { xs: 1, sm: 1 },
-        },
-      ]}
-    >
-      <Typography
-        sx={{ flex: '1 1 100%' }}
-        variant="h6"
-        id="tableTitle"
-        component="div"
-      >
-        Nutrition
-      </Typography>
-      <Tooltip title="Filter list">
-        <IconButton>
-          <FilterListIcon />
-        </IconButton>
-      </Tooltip>
-    </Toolbar>
-  );
-}
+
 export default function EnhancedTable() {
+  const [rows, setRows] = React.useState<Record<string, string | number | null>[]>([]);
   const [order, setOrder] = React.useState<Order>('asc');
-  const [orderBy, setOrderBy] = React.useState<keyof Data>('calories');
+  const [orderBy, setOrderBy] = React.useState<string>();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
   const handleRequestSort = (
     _event: React.MouseEvent<unknown>,
-    property: keyof Data,
+    property: string,
   ) => {
     const isAsc = orderBy === property && order === 'asc';
     setOrder(isAsc ? 'desc' : 'asc');
@@ -221,13 +119,26 @@ export default function EnhancedTable() {
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
+  const sortedRows = React.useMemo(
+    () =>
+      orderBy
+        ? [...rows].sort(getComparator(order, orderBy))
+        : [...rows],
+    [order, orderBy, rows],
+  );
+
   const visibleRows = React.useMemo(
     () =>
-      [...rows]
-        .sort(getComparator(order, orderBy))
-        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
-    [order, orderBy, page, rowsPerPage],
+      [...sortedRows].filter((row) => !row.parentId).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+    [page, rowsPerPage, sortedRows],
   );
+
+  const headCells: HeadCell[] = rows.length ? Object.keys(rows[0]).map((key) => ({
+    id: key,
+    numeric: false,
+    disablePadding: true,
+    label: key,
+  })) : [];
 
   return (
     <Box sx={{ width: '100%' }}>
@@ -237,14 +148,57 @@ export default function EnhancedTable() {
           const buffer = reader.result;
           const workbook = XLSX.read(buffer);
           const ws = workbook.Sheets[workbook.SheetNames[0]];
-          const data = XLSX.utils.sheet_to_json(ws);
+          const data: Record<string, string | number>[] = XLSX.utils.sheet_to_json(ws);
           console.log('data', data);
+          let treeData: Record<string, string | number | null>[] = [];
+          let previousMealIndex = 0;
+          let previousDayIndex = 0;
+          data.forEach((row, index) => {
+            if (!row.meal) {
+              treeData = [
+                ...treeData.slice(0, previousDayIndex),
+                ...treeData.slice(previousDayIndex).map((previousRow) =>
+                  previousRow.foodid
+                    ? previousRow
+                    : {
+                        ...previousRow,
+                        parentId: index + 1,
+                      },
+                ),
+                {
+                  id: index + 1,
+                  parentId: null,
+                  ...row,
+                },
+              ];
+              previousDayIndex = index + 1;
+              previousMealIndex = index + 1;
+            } else if (!row.foodid) {
+              treeData = [
+                ...treeData,
+                ...data
+                  .slice(previousMealIndex, index)
+                  .map((foodRow, foodIndex) => ({
+                    id: previousMealIndex + foodIndex + 1,
+                    parentId: index + 1,
+                    ...foodRow,
+                  })),
+                {
+                  id: index + 1,
+                  parentId: null,
+                  ...row,
+                },
+              ];
+              previousMealIndex = index + 1;
+            }
+          });
+          console.log('treeData', treeData);
+          setRows(treeData);
         };
         reader.addEventListener('load', readFile);
         reader.readAsArrayBuffer(event.target.files![0]);
       }}/>
       <Paper sx={{ width: '100%', mb: 2 }}>
-        <EnhancedTableToolbar />
         <TableContainer>
           <Table
             sx={{ minWidth: 750 }}
@@ -252,33 +206,49 @@ export default function EnhancedTable() {
             size="small"
           >
             <EnhancedTableHead
+              headCells={headCells}
               order={order}
               orderBy={orderBy}
               onRequestSort={handleRequestSort}
               rowCount={rows.length}
             />
             <TableBody>
-              {visibleRows.map((row, index) => {
-                const labelId = `enhanced-table-checkbox-${index}`;
-
+              {visibleRows.map((row) => {
                 return (
-                  <TableRow
-                    tabIndex={-1}
-                    key={row.id}
-                  >
-                    <TableCell
-                      component="th"
-                      id={labelId}
-                      scope="row"
-                      padding="none"
-                    >
-                      {row.name}
-                    </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
-                  </TableRow>
+                  <>
+                    <TableRow key={row.id}>
+                      <TableCell>
+                        <ArrowUpward />
+                      </TableCell>
+                      {headCells.map((headCell) => (
+                        <TableCell>{row[headCell.id]}</TableCell>
+                      ))}
+                    </TableRow>
+                    {sortedRows
+                      .filter((meal) => meal.parentId === row.id)
+                      .map((meal) => (
+                        <>
+                          <TableRow key={meal.id} sx={{ pl: 4 }}>
+                            <TableCell>
+                              <ArrowUpward />
+                            </TableCell>
+                            {headCells.map((headCell) => (
+                              <TableCell>{meal[headCell.id]}</TableCell>
+                            ))}
+                          </TableRow>
+                          {sortedRows
+                            .filter((food) => food.parentId === meal.id)
+                            .map((food) => (
+                              <TableRow key={food.id} sx={{ pl: 4 }}>
+                                <TableCell />
+                                {headCells.map((headCell) => (
+                                  <TableCell>{food[headCell.id]}</TableCell>
+                                ))}
+                              </TableRow>
+                            ))}
+                        </>
+                      ))}
+                  </>
                 );
               })}
               {emptyRows > 0 && (
@@ -294,7 +264,7 @@ export default function EnhancedTable() {
           </Table>
         </TableContainer>
         <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
+          rowsPerPageOptions={[5, 10, 25, 10000]}
           component="div"
           count={rows.length}
           rowsPerPage={rowsPerPage}
