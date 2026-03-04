7fc9bd0ba236a4ee8f71f18e3940965165b45ffb
Teemu Orava <teemu.orava@kapsi.fi>
2026-03-04 13:25:45 +0200

    Add Chromium args

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
 import react from '@vitejs/plugin-react';
+import { webdriverio } from '@vitest/browser-webdriverio';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,7 +9,18 @@ export default defineConfig({
     browser: {
       enabled: true,
       provider: playwright(),
-      instances: [{ browser: 'chromium' }],
+      instances: [
+        {
+          browser: 'chromium',
+          provider: webdriverio({
+            capabilities: {
+              'goog:chromeOptions': {
+                args: ['disable-gpu', 'no-sandbox', 'disable-setuid-sandbox'],
+              },
+            },
+          }),
+        },
+      ],
     },
   },
 });
